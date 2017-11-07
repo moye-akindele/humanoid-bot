096fcb40b1e200997d47c01f8ff0fef326b74233
moye-akindele <akinmoye2000@hotmail.co.uk>
2017-11-07 21:10:47 +0000

    Add files via upload

--- HumanoidBot/HumanoidBot/ConfigData/AppBox.cs
+++ HumanoidBot/HumanoidBot/ConfigData/AppBox.cs
@@ -1,1 +1,13 @@
-
+﻿namespace Pca9685Test.ConfigData
+{
+    public class AppBox
+    {
+        //File paths
+        public const string motionDataPath = @"ServoPosition\motion.json";
+        
+        //Commands
+        public const string MotionStop = "Stop";
+        public const string MotionWalk = "Walk";
+        public const string MotionStand = "stand";
+    }
+}
